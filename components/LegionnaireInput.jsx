3eeb91a294b470cc9e87ca0409d1862c8800e8d9
Adam Ryan <adamryan1983@gmail.com>
2021-02-17 11:31:37 -0330

    basic functionality working

---
+++ components/LegionnaireInput.jsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+import styles from '../styles/inputSection.module.scss'
+
+//prime react imports
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import 'primereact/resources/themes/mdc-light-deeppurple/theme.css';
+import 'primereact/resources/primereact.css';
+import PrimeReact from 'primereact/api';
+
+function LegionnaireInput(props) {
+
+  let note, reason;
+  const [tempStatus, setTempStatus] = useState()
+
+  const handleSubmit = () => {
+    tempStatus ? showSuccess() : showFail();
+    setTempStatus(null)
+  }
+
+  const handleStatusChange = (event) => {
+    setTempStatus(event.target.value)
+    // props.setStatus(event.target.value);
+  };
+  const handleReasonChange = (event) => {
+    reason = event.target.value
+    // props.setReason(event.target.value);
+  };
+  const handleNoteChange = (event) => {
+    note = event.target.value
+    // props.setNote(event.target.value);
+  };
+
+  const returnBack = () => {
+    props.setflandersisVisible(false)
+    props.setlegionnaireisVisible(false)
+  }
+
+  const showSuccess = () => {
+    props.toast.current.show({severity:'success', summary: 'Success', detail:'Status Updated', life: 3000});
+    props.setReason1(reason);
+    props.setStatus1(tempStatus);
+    props.setNote1(note);
+    document.querySelector("#notesTextBox1").value = ""
+    document.querySelector("#reasonTextBox1").value = ""
+}
+  const showFail = () => {
+    props.toast.current.show({severity:'error', summary: 'Fail', detail:'Status Failed to Update', life: 3000});
+}
+
+  return (
+    <div className={styles.boatBlock}>
+      <div className={styles.dropInput}>
+        Legionnaire Boat Status: 
+        <Dropdown value={tempStatus} options={props.statusSelectItems} onChange={handleStatusChange} optionLabel="label" placeholder="Select Boat Status"/>
+      </div>
+      <div className={styles.reasonInput}>
+          <label htmlFor="reasonTextBox1">Reason: </label>
+          <InputText id="reasonTextBox1" onChange={handleReasonChange} required/>
+
+      </div>
+      <div className={styles.notesInput}>
+          <label htmlFor="notesTextBox1">Notes: </label>
+          <InputTextarea id="notesTextBox1" rows={2} cols={30} onChange={handleNoteChange} required/>
+
+      </div>
+      <div className={styles.submitButton}>
+        <Button onClick={ handleSubmit }>
+          Submit
+        </Button>   
+        <Button onClick={returnBack} style={{backgroundColor:'red'}}>Back</Button>
+      </div>
+    </div>
+  )
+}
+
+export default LegionnaireInput
