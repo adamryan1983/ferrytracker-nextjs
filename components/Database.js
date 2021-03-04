527671b25431d6a50ce6f09790a5c3012fe35b4e
Adam Ryan <adamryan1983@gmail.com>
2021-03-04 00:00:55 -0330

    fixed env

--- components/Database.js
+++ components/Database.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  apiKey: process.env.apiKey,
   authDomain: "ferry-app-7d4d2.firebaseapp.com",
   databaseURL: "https://ferry-app-7d4d2-default-rtdb.firebaseio.com",
   projectId: "ferry-app-7d4d2",
   storageBucket: "ferry-app-7d4d2.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appID,
-  measurementId: process.env.NEXT_PUBLIC_measurementId,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appID,
+  measurementId: process.env.measurementId,
 };
 
 if (!firebase.apps.length) {
