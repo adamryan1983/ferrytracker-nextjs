98b8681c8d7eacd00987be60c92661cbe7c5d952
Adam Ryan <adamryan1983@gmail.com>
2021-03-03 23:43:44 -0330

    fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
 import "../styles/globals.css";
 import PrimeReact from "primereact/api";
-import { HarperDBProvider } from "use-harperdb";
 
 function MyApp({ Component, pageProps }) {
   // active ripple effect
   PrimeReact.ripple = true;
-  <HarperDBProvider
-    url={process.env.REACT_APP_DB_URL}
-    user={process.env.REACT_APP_USER}
-    password={process.env.REACT_APP_PASSWORD}
-  ></HarperDBProvider>;
   return <Component {...pageProps} />;
 }
 
